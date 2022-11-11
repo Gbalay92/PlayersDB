758ffb788db0ce7ddb18a255cee50071fb42e16d
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2022-11-11 10:06:58 +0100

    add DatabaseClass

---
+++ app/src/main/java/com/liceolapaz/dam/gbl/JugadoresSql.kt
@@ -0,0 +1,26 @@
+package com.liceolapaz.dam.gbl
+
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class JugadoresSql(context: Context?,name: String?,factory: SQLiteDatabase.CursorFactory?,version: Int)
+    : SQLiteOpenHelper(context, name, factory, version) {
+    //Sentencia SQL para crear la tabla de Usuarios
+    val sqlCreate = "CREATE TABLE Jugadores (codigo INTEGER, nombre TEXT, precio REAl, posicion TEXT, puntos INTEGER)"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        //Se ejecuta la sentencia SQL de creación de la tabla
+        db?.execSQL(sqlCreate)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, previousVersion: Int, newVersion: Int) {
+
+        //Se elimina la versión anterior de la tabla
+        db?.execSQL("DROP TABLE IF EXISTS Usuarios")
+
+        //Se crea la nueva versión de la tabla
+        db?.execSQL(sqlCreate)
+    }
+}
--- app/src/main/java/com/liceolapaz/dam/gbl/MainActivity.kt
+++ app/src/main/java/com/liceolapaz/dam/gbl/MainActivity.kt
@@ -2,10 +2,41 @@ package com.liceolapaz.dam.gbl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var user: EditText
+    private lateinit var password: EditText
+    private lateinit var txtError: TextView
+    private lateinit var login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        user=findViewById(R.id.inputUser)
+        password=findViewById(R.id.inputPassword)
+        txtError=findViewById(R.id.txtError)
+        login=findViewById(R.id.login)
+
+
+        login.setOnClickListener {
+            if(user.text.toString()=="admin" && password.text.toString()=="liceo"){
+                txtError.text = "yuju"
+                val jugdb=JugadoresSql(this, "Database", null, 1)
+                val db = jugdb.writableDatabase
+
+            }
+            else{
+                txtError.text="Usuario y/o contraseña incorrectos"
+            }
+
+        }
+
+
+
+
     }
 }
